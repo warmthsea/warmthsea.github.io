a3075de97122e0bcac4e0eedb68ad67458c4cfbb
Wanghua3819 <2586244885@qq.com>
2022-06-22 21:38:05 +0800

    update

--- assets/AboutView.6ce45183.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as s,a as t}from"./index.1f3b3ca5.js";const a={},c={class:"about"},_=t("h1",null,"This is an about page",-1),n=[_];function r(i,d){return o(),s("div",c,n)}var u=e(a,[["render",r]]);export{u as default};
